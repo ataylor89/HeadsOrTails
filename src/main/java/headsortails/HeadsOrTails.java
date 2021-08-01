236c89e4bba4e9c01124f01cf17a53f7cbb57389
Andrew <rolyata47@gmail.com>
2021-07-31 22:05:20 -0500

    First commit

---
+++ src/main/java/headsortails/HeadsOrTails.java
@@ -0,0 +1,12 @@
+package headsortails;
+
+/**
+ *
+ * @author andrewtaylor
+ */
+public class HeadsOrTails {
+    public static void main(String[] args) {
+        HeadsOrTailsJFrame window = new HeadsOrTailsJFrame();
+        window.setVisible(true);
+    }
+}
